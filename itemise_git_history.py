b95c642abb8db813eb7ce6540efce37c3b04e189
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2019-09-28 19:23:30 +0100

    Trim uneeded dates

--- itemise_git_history.py
+++ itemise_git_history.py
@@ -121,56 +121,6 @@ path_map = {
 }
 
 redate_map = {
-  "3006b4492c7877b853a00eab339dda2033479396": "2019-05-11T04:57:42.413Z", # Products/BonusBin
-  "b4ce2a51605719cd3724d9ad5d5383e16f169441": "2019-05-11T04:57:55.955Z", # Products/Disc
-  "a0aa4e580bca75b9b084134dfcf96f031eb96209": "2019-05-11T04:58:20.710Z", # Products/PlingSystem
-  "a8101de3ad4f3d531367542bf48449d5cf00c368": "2019-05-11T09:45:33.349Z", # RiscOS/Tools/Sources/GNU/bison
-  "36ed95f7a6b73d7bf24d34bfe8fcdfc824c08114": "2019-05-11T09:50:41.584Z", # RiscOS/Sources/Utilities/Connector
-  "b255700bdc7a9fe05c26282f7ae3afd039be5f22": "2019-05-11T10:00:38.154Z", # RiscOS/Tools/Sources/GNU/defmod
-  "9894f4371bb02a3c36dea387614a03ae385ae543": "2019-05-11T10:01:58.431Z", # RiscOS/Sources/Lib/Desk
-  "4985ff76f698cbca6f220784a82e9f09ec21a424": "2019-05-11T10:03:19.514Z", # RiscOS/Sources/HWSupport/USB/Controllers/EHCIDriver
-  "cfb2e2913277fdaa3e50c845f406e4618584ee4b": "2019-05-11T10:03:27.546Z", # RiscOS/Sources/Networking/Ethernet/EtherB
-  "6b3f215263ec6f2ce50c79d40bab624b13b8e8c7": "2019-05-11T10:03:36.043Z", # RiscOS/Sources/Networking/Ethernet/EtherCPSW
-  "959eaa618bc134fc644cf46802401ad01477e7d3": "2019-05-11T10:04:07.101Z", # RiscOS/Sources/Networking/Ethernet/EtherY
-  "cc29259315f7961350ff23005e8b1561b42ba496": "2019-05-11T10:05:05.182Z", # RiscOS/Tools/Sources/GNU/flex
-  "8dfe324391512209ca249afe02fb463d0378d646": "2019-05-11T10:05:19.488Z", # RiscOS/Tools/Sources/GNU/gawk
-  "482b6a1dc55a012c6f4366e3c98971d59bb3a8d5": "2019-05-11T10:05:28.959Z", # RiscOS/Tools/Sources/GNU/gnudiff
-  "13d9a0d672689ed885f1483040d8b93052f5dccf": "2019-05-11T10:11:29.869Z", # RiscOS/Tools/Sources/GNU/grep
-  "c2945bb2d96d6c606153e815fca6940d8dfdd779": "2019-05-11T10:11:37.065Z", # RiscOS/Tools/Sources/GNU/ident
-  "d6d4c8a780a45acbbfbf737ffd95ccc580f3c1ae": "2019-05-11T10:12:38.691Z", # RiscOS/Sources/Lib/ImageLib
-  "e6547e2615d582f148599b583a5d4b99e242d7ff": "2019-05-11T10:13:05.469Z", # RiscOS/Sources/Video/HWSupport/IMXVideo
-  "0004e1f85f38461924728a42d70b5e9eb23213b7": "2019-05-11T10:15:20.288Z", # RiscOS/Sources/Video/Render/JCompMod
-  "73fb7629d2f9fd621b83156d2cf2551d355df77f": "2019-05-11T10:15:33.560Z", # RiscOS/Tools/Sources/GNU/libgnu4
-  "44bb8972b4e70e8ffae79094d69b4235f2981c84": "2019-05-11T10:15:49.127Z", # RiscOS/Tools/Sources/GNU/libgnu
-  "e5f8f7b49c51cf599b2a077d21e68935b17fa389": "2019-05-11T10:16:38.824Z", # RiscOS/Sources/Lib/mbedTLS
-  "b18e105a06dbfa32914ec327a885a774bc925560": "2019-05-11T10:17:03.613Z", # RiscOS/Sources/HWSupport/USB/Controllers/OHCIDriver
-  "daf518f90dea7c24ad858b0f74b390be6836c9db": "2019-05-11T10:17:27.832Z", # RiscOS/Sources/Lib/OSLib
-  "55eb4abc55cde0e4c58d3447f3e1db09303f4d36": "2019-05-11T10:17:57.534Z", # RiscOS/Utilities/Perl
-  "9a8aee3cdb9c5dd4a3f5ae7dc2700380397e3dfe": "2019-05-11T10:18:06.785Z", # RiscOS/Tools/Sources/GNU/readelf
-  "d1465d72bb7ea93d93e1f4fd516c50a41d24cf2a": "2019-05-11T10:18:15.348Z", # RiscOS/Sources/Networking/AUN/RouteD
-  "319fea697572f34e7d8c70c7af0c22fd2cfe4fbb": "2019-05-11T10:18:38.220Z", # RiscOS/Tools/Sources/GNU/sed
-  "84ad545c8c98ad847bcafa8ae8a7a66632fd285e": "2019-05-11T10:19:22.804Z", # RiscOS/Sources/Video/Render/SprExtend
-  "fedfb7075eca59d2c873f258d90092988f035dc0": "2019-05-11T10:29:58.620Z", # RiscOS/Sources/HWSupport/VCHIQ
-  "fc843a66ef4651b6d5eb3d0361dc7ffdbae5aab8": "2019-05-11T10:30:10.502Z", # RiscOS/Sources/HWSupport/VFPSupport
-  "296bfe0862e29b15d936d2408768f1f4fdb7814f": "2019-05-11T10:30:36.111Z", # RiscOS/Sources/Lib/zlib
-  "e06361066c74f0ad10d84d6f40cdd9c1aadbac7f": "2019-05-11T13:59:51.247Z", # Products/All
-  "9b15a8d7217faaab83202987d714118a24e21f3a": "2019-05-11T14:00:04.065Z", # Products/Batch1to6
-  "77e6ff5ec215906cfad66c994cdc57df2774f9d6": "2019-05-11T14:00:10.832Z", # Products/BCM2835
-  "46bcff2948819bd199ab830ef680d183d24703d2": "2019-05-11T14:00:18.101Z", # Products/BCM2835Pico
-  "1ffd242594342117e45b0357d6b4086e45b64514": "2019-05-11T14:00:24.103Z", # Products/BonusBin
-  "ad179f963a9250e4626fd68f2377a88398443e7b": "2019-05-11T14:00:29.204Z", # Products/BuildHost
-  "dbe40f6e7f9426678e25b8f2e421f4ad35637fcf": "2019-05-11T14:00:42.891Z", # Products/Disc
-  "11347cf5c2cd20d605c75c01f2c2d2d399e7d049": "2019-05-11T14:00:49.802Z", # Products/iMx6
-  "95f44d09a7680963b0d0a7ec3e1151d60cab0ed0": "2019-05-11T14:00:55.393Z", # Products/IOMDHAL
-  "050926a51786d4aedf2c993c03eae44846ad6655": "2019-05-11T14:01:02.892Z", # Products/OMAP3
-  "d71cc6a7b377ca203bf4a2b63e5876cee0ab3d96": "2019-05-11T14:01:10.559Z", # Products/OMAP4
-  "ace3d0acdbde1b7412d28f6c184e9df8ed6fcb72": "2019-05-11T14:01:16.697Z", # Products/OMAP5
-  "b08c52798452894c648f32d3852b1d6a112a222f": "2019-05-11T14:01:21.801Z", # Products/PlingSystem
-  "df5b7fa8992c3041c3d6513b6d8290b2c1491e57": "2019-05-11T14:01:26.788Z", # Products/S3C
-  "9995499ef13e4886281d69da7b658aa0f5cc0792": "2019-05-11T14:01:32.304Z", # Products/Titanium
-  "829275201b89da333632998b21c243eb50e52e02": "2019-05-11T14:01:39.816Z", # Products/Tungsten
-  "ee6a4a83c4ad32bf0fa52c324f65fcadb3b3d922": "2019-05-14T16:40:27.473Z", # Products/Titanium
-  "73534cde8cc60fc1b896f968d31a4f09d0166e40": "2019-05-23T05:17:25.152Z", # RiscOS/Sources/SystemRes/InetRes
   "2335669ef0fd2330a2a9f02c807917d0d4de4f6e": "2019-05-23T11:41:40.732Z", # RiscOS/Library
   "16d75e8bed69636d571d1652a135b439c006dc1e": "2019-05-23T14:08:43.134Z", # RiscOS/BuildSys
   "e66f346e23654c15154efef6c8152bc3cc9bf521": "2019-05-24T07:07:49.948Z", # RiscOS/Apps/PlingPrepare
